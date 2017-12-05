e976b000084a273125142931face05e92d76fc78
Kuang Ru <rukuang@ubuntu16x64.example.com>
2017-12-05 19:16:10 +0800

    不是sudoer直接退出.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #该脚本在ubuntu 16.04 server可运行.
-sudo apt install pylint3 ctags python-dev cmake g++ xz-utils
+sudo apt install pylint3 ctags python-dev cmake g++ xz-utils || exit 1;
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp google_style.pylint.rc ~
 cp vimrc ~/.vimrc
